fc467a9d1807a0778d63de4f1e3214f7b9eb9f63
wuyi <2644258557@qq.com>
2021-09-26 18:39:43 +0800

    第二个网站 跨域

---
+++ public/frank.js
@@ -0,0 +1,26 @@
+function jsonp(url){
+    return new Promise((resolve,reject)=>{
+    const random =Math.random()
+    console.log(random)
+    window[random] =(data)=>{
+        resolve(data);
+    };
+    const script = document.createElement('script')
+    script.src =`${url}?callback=${random}`
+    script.onload =()=>{
+        script.remove();
+    };
+    script.onerror=()=>{
+        reject();
+    };
+    document.body.appendChild(script)
+  });
+}
+jsonp('http://qq.com:8888/friends.js')
+    .then((data)=>{
+        console.log(data)
+    })
+
+
+
+
